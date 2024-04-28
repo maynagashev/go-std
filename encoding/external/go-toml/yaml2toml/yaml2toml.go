aa4b61d09898804aa06ee6c5604967635e7950b7
Eugene Mainagashev <lifesup@gmail.com>
2024-04-28 20:06:49 +0700

    Использование go-toml  и пример yaml2toml

--- encoding/external/go-toml/yaml2toml/yaml2toml.go
+++ encoding/external/go-toml/yaml2toml/yaml2toml.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/pelletier/go-toml"
+	"gopkg.in/yaml.v3"
+)
+
 type Data struct {
 	ID     int    `toml:"id"`
 	Name   string `toml:"name"`
@@ -18,7 +24,16 @@ values:
 func main() {
 	// вставьте недостающий код
 	// 1) десериализуйте yamlData в переменную типа Data
+	var d Data
+	err := yaml.Unmarshal([]byte(yamlData), &d)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("%#v\n", d)
+
 	// 2) преобразуйте полученную переменную в TOML
+	encoded, err := toml.Marshal(d)
+
 	// 3) выведите в консоль результат
-	// ...
+	fmt.Println(string(encoded))
 }
