bba59c3d7ef0dbcefecbdfc240621bc5b2f440a8
Maynagashev <lifesup@gmail.com>
2025-01-19 09:25:20 +0700

    Два бенчмарка в рамках одной функции с помощью b.Run

--- _testing/benchmarks/1_fibonacci/fibonacci_test.go
+++ _testing/benchmarks/1_fibonacci/fibonacci_test.go
@@ -3,7 +3,15 @@ package main
 import "testing"
 
 func BenchmarkFibo(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		FiboRecursive(20)
-	}
+	count := 20
+	b.Run("recursive", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			FiboRecursive(count)
+		}
+	})
+	b.Run("optimized", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			FiboOptimized(count)
+		}
+	})
 }
