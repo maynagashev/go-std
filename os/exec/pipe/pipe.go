24cba45e450db5315f5e40db5fde3d2fa71b4b3f
Maynagashev <lifesup@gmail.com>
2025-03-07 01:03:04 +0700

    Упрощенная версия

--- os/exec/pipe/pipe.go
+++ os/exec/pipe/pipe.go
@@ -2,11 +2,9 @@
 package main
 
 import (
-	"io"
 	"log"
 	"os"
 	"os/exec"
-	"sync"
 )
 
 func main() {
@@ -17,31 +15,14 @@ func main() {
 	}
 
 	cmdin := exec.Command("cat")
-	// указываем текущую консоль для стандартного вывода
 	cmdin.Stdout = os.Stdout
-	stdin, err := cmdin.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var wg sync.WaitGroup
-	wg.Add(1)
+	cmdin.Stdin = stdout
+
 	if err = cmdout.Start(); err != nil {
 		log.Fatal(err)
 	}
 	if err = cmdin.Start(); err != nil {
 		log.Fatal(err)
 	}
-
-	go func() {
-		// перенаправляем потоки данных
-		if _, err = io.Copy(stdin, stdout); err != nil {
-			log.Fatal(err)
-		}
-		wg.Done()
-		// закрываем, чтобы завершился процесс cat
-		stdin.Close()
-	}()
-	wg.Wait()
-	cmdout.Wait()
 	cmdin.Wait()
 }
